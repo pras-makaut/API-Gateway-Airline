e5cb6994e4163b174e46b06b8d793cbbeafac53a
Ranjan <prasranjan@deloitte.com>
2023-11-23 05:31:25 +0530

    added encrytion in signup

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,8 @@
 const {
   Model
 } = require('sequelize');
+const bcrypt = require("bcryptjs")
+const {ServerConfig} = require('../config')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -33,5 +35,9 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+  User.beforeCreate(function encrypt(user){
+    const encryptedPassword = bcrypt.hashSync(user.password,ServerConfig.SALT_ROUNDS);
+    user.password = encryptedPassword;
+  })
   return User;
 };
