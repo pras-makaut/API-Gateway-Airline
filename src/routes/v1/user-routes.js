08c118746f91ab8b693f764b927183fb212870a8
Ranjan <prasranjan@deloitte.com>
2023-11-26 07:38:31 +0530

    added middlewares of auth

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const {UserController} = require('../../controllers')
 const router = express.Router();
 
-// api/v1/users POST
-router.post('/signup',UserController.signup);
+const {AuthRequestMiddlewares} = require('../../middleware');
 
-router.post('/signin',UserController.signin);
+// api/v1/users POST
+router.post('/signup',AuthRequestMiddlewares.validateAuthRequest,UserController.signup);
+
+router.post('/signin',AuthRequestMiddlewares.validateAuthRequest,UserController.signin);
 
 
 
